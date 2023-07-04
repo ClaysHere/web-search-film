8c2777226f5afbba80799647e24419040a49f09c
ClaysHere <muhammadfikri4402@gmail.com>
2023-07-05 01:06:59 +0700

    responsif

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const App = () => {
     return popularMovies.map((movie, i) => {
       return (
         <div
-          className="w-1/4 flex flex-col items-center p-8 bg-slate-400 text-slate-800 font-semibold gap-y-4 rounded-xl"
+          className="lg:w-1/4 w-full flex flex-col items-center p-8 bg-slate-400 text-slate-800 font-semibold gap-y-4 rounded-xl"
           key={i}>
-          <div className="h-[50px] text-center">{movie.title}</div>
+          <div className="lg:h-[50px] text-center">{movie.title}</div>
           <img
-            className="w-[350px] h-[500px] rounded-lg"
+            className="lg:w-[350px] lg:h-[500px] rounded-lg"
             src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
             alt="gambar"
           />
@@ -48,10 +48,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className={"w-full flex flex-col items-center gap-8 p-16"}>
+      <header className={"w-full flex flex-col items-center gap-8 p-4 lg:p-16"}>
         <h1
           className={
-            "text-6xl font-bold font-sans text-white bg-slate-500 p-4 rounded-lg"
+            "lg:text-6xl font-bold font-sans text-white bg-slate-500 p-4 rounded-lg"
           }>
           My Movie Mania
         </h1>
@@ -59,17 +59,17 @@ const App = () => {
         <div className="w-full flex justify-center gap-4">
           <input
             placeholder="Search Film"
-            className={"w-1/2 p-4 text-xl rounded-xl"}
+            className={"lg:w-1/2 p-4 text-xl rounded-xl"}
             // onChange={({ target }) => search(target.value)}
             ref={searchRef}
           />
           <button
             onClick={() => search(searchRef.current.value)}
-            className="bg-teal-300 px-8 rounded-2xl text-slate-700 font-bold text-xl hover:bg-teal-600 hover:text-slate-300 active:bg-slate-600">
+            className="bg-teal-300 px-4 lg:px-8 rounded-2xl text-slate-700 font-bold lg:text-xl hover:bg-teal-600 hover:text-slate-300 active:bg-slate-600">
             Search
           </button>
         </div>
-        <div className="w-full flex flex-wrap gap-16 justify-center">
+        <div className="w-full gap-y-8 flex flex-wrap lg:gap-16 justify-center">
           <PopularMoviesList />
         </div>
       </header>
